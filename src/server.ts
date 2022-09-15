6c4182046dabfbb3187dff16b17e7cdd3562c371
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-09-15 12:15:05 -0300

    add cors and convert time

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinuts } from "./utils/convert-hour-string-to-minuts";
+import { convertMinutesToHourString } from "./utils/convert-minutes-to-hour-string";
 
 const app = express()
-app.use(express.json())
 const prisma = new PrismaClient()
 
+app.use(express.json())
+app.use(cors({}))
 
 app.get('/games', async (req, resp) => {
     const games = await prisma.game.findMany({
@@ -19,17 +23,29 @@ app.get('/games', async (req, resp) => {
     return resp.json(games)
 })
 
-app.post('/games/:id/ads', (req, resp) => {
+app.post('/games/:id/ads', async (req, resp) => {
     const gameId = req.params.id
-    const body = req.body
-    console.log(body)
+    const body: any = req.body
 
-    return resp.status(201).json(body)
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinuts(body.hourStart),
+            hourEnd: convertHourStringToMinuts(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel
+        }
+    })
+
+    return resp.status(201).json(ad)
 })
 
 app.get('/games/:id/ads', async (req, resp) => {
     const gameId = req.params.id;
-    const ads = await prisma.ad.findMany({
+    const ads:any = await prisma.ad.findMany({
         select: {
             id: true,
             name: true,
@@ -47,10 +63,12 @@ app.get('/games/:id/ads', async (req, resp) => {
         }
     })
     
-    return resp.json(ads.map(ad => {
+    return resp.json(ads.map((ad:any) => {
         return {
             ...ad,
             weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd)
         }
     }))
 })
